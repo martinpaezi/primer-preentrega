8667e8a95ae6b6ba4198afd6301cdde479024cd9
martinpaezi <paezmartin99@gmail.com>
2023-12-11 18:48:09 -0300

    Modifiqué texto y agregué saludo final

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,15 +1,15 @@
 let volverAIntentar = true
 let nombre = prompt("¿Cómo te llamas?");
 
-alert(`Bienvenido ${nombre}`);
+alert(`¡Bienvenido ${nombre}!`);
   
 function determinarEquipo() {
   alert("Vamos a ver de qué equipo deberías ser hincha en el fútbol argentino.");
 
   let valores = prompt("¿Te representan tus valores? (Responde 's' para sí, 'n' para no)");
   let personalidad = prompt("¿Tienes una personalidad fuerte? (Responde 's' para sí, 'n' para no)");
-  let importaGanar = prompt("¿Te importa solamente ganar a cualquier costo, hasta quizas sobornando a un árbitro? (Responde 's' para sí, 'n' para no)");
-  let jugarBien = prompt("¿Te importa jugar bien mas que ganar o perder? (Responde 's' para sí, 'n' para no)");
+  let importaGanar = prompt("¿Te importa solamente ganar a cualquier costo, hasta quizas de una manera poco honesta? (Responde 's' para sí, 'n' para no)");
+  let jugarBien = prompt("¿Te importa mas jugar bien, que ganar o perder? (Responde 's' para sí, 'n' para no)");
 
   if (valores.toLowerCase() === 's' && personalidad.toLowerCase() === 's' && importaGanar.toLowerCase() === 'n' && jugarBien.toLowerCase() === 'n') {
       alert("Deberías ser hincha de Estudiantes.");
@@ -34,3 +34,5 @@ if(otroIntento.toLowerCase() === 's'){
   volverAIntentar = false;
 }
 }
+
+alert(`¡Hasta luego ${nombre}!`)
